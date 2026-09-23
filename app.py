b50db65808cfd14bccf36b5e4b56485d6590ba2e
Jesse <100-LiesJC@://github.com>
2026-09-22 22:18:30 -0400

    Jinja y cookies

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+#NOMBRE : JESSE CASAS CHURQUI   MATERIA: TECNOLOGIAS EMERGENTES II
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 app = Flask(__name__)
 
 app.secret_key = '8dbe7b26953e32807b5edc08c9f0614d1e067ce92d7c943663be74acfdd09f17'
@@ -11,7 +12,9 @@ usuarios = {
 
 @app.route('/')
 def index():
-    return render_template('index.html', titulo="Portal Académico")
+    cookie_usuario = request.cookies.get('usuario_preferido')
+    cerrado = request.args.get('logout_exitoso')
+    return render_template('index.html', titulo="Portal Académico", cookie_usuario=cookie_usuario, cerrado=cerrado)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,12 +24,13 @@ def login():
         
         if u in usuarios and usuarios[u] == p:
             session['usuario'] = u
-            return redirect(url_for('cursos'))
+            response = make_response(redirect(url_for('cursos')))
+            response.set_cookie('usuario_preferido', u)
+            return response
         else:
             return render_template('login.html', error="Usuario o contraseña incorrectos.")
             
     return render_template('login.html')
-
 
 @app.route('/cursos')
 def cursos():
@@ -47,7 +51,13 @@ def perfil():
 @app.route('/logout')
 def logout():
     session.pop('usuario', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('index', logout_exitoso=1))
+
+@app.route('/eliminar-cookie')
+def eliminar_cookie():
+    response = make_response(redirect(url_for('index')))
+    response.delete_cookie('usuario_preferido')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
